1b37d54fa3966e976116b7c7035c4c92d0975401
Tristan <atdyer@ncsu.edu>
2016-12-06 13:39:13 -0500

    Finish up

--- courses/ce594/project/pdf/gen_pdfs.py
+++ courses/ce594/project/pdf/gen_pdfs.py
@@ -2,7 +2,8 @@ import os
 
 # Don't forget to comment out any letter spacing in the css files
 os.system( 'wkhtmltopdf ../index.html project.pdf')
-# os.system( 'wkhtmltopdf ../shape_functions.html code_shape_functions.pdf' )
-# os.system( 'wkhtmltopdf ../project.html code_project.pdf' )
-# os.system( 'wkhtmltopdf ../jacobian.html code_jacobian.pdf' )
-# os.system( 'wkhtmltopdf ../fe.html code_fe.pdf')
+os.system( 'wkhtmltopdf ../shape_functions.html code_shape_functions.pdf' )
+os.system( 'wkhtmltopdf ../project.html code_project.pdf' )
+os.system( 'wkhtmltopdf ../jacobian.html code_jacobian.pdf' )
+os.system( 'wkhtmltopdf ../fe.html code_fe.pdf')
+os.system( 'pdftk project.pdf written_project.pdf code_project.pdf code_fe.pdf code_jacobian.pdf code_shape_functions.pdf cat output dyer_final_project.pdf')
